66f024dae60097ab8f4adfc9a93d976911adddd7
Adrian <bronkic@gmail.com>
2020-03-29 12:30:31 +0200

    Add files via upload
    initial commit

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mail_to = "bronkic@gmail.com";
+    $subject = "Contact form message from: ".$name;
+    $txt = "You have received a message via contact form from ".$name.".\n\n".$message;
+
+    mail($mail_to, $subject, $txt);
+    header("Location: index.php?mailsend");
+}
