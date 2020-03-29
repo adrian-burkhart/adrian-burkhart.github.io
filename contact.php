09bc5f0877c55d2f1eee2403ffaf36b987664e64
unknown <adrian.burkhart@gmail.com>
2020-03-29 15:42:00 +0200

    Fixed contact formular

--- contact.php
+++ contact.php
@@ -3,13 +3,14 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $email = $_POST['email'];
+    $mail = $_POST['mail'];
     $message = $_POST['message'];
 
     $mail_to = "a.burkhart@web-netz.de";
+    $headers = "From: ".$mail;
     $subject = "Contact form message from: ".$name;
     $txt = "You have received a message via contact form from ".$name.".\n\n".$message;
 
-    mail($mail_to, $subject, $txt);
-    header("Location: index.php?mailsend");
+    mail($mail_to, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
 }
